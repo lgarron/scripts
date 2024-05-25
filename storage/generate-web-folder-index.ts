71262d17fdae58f0be067f7477afc84770cf1a46
Lucas Garron <code@garron.net>
2024-05-24 19:32:40 -0700

    [generate-web-folder-index] Update formatting and automatically list videos.

--- storage/generate-web-folder-index.ts
+++ storage/generate-web-folder-index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 
-import { argv } from "bun";
+import { Glob, argv } from "bun";
 import { JSDOM } from "jsdom";
 
 const dom = new JSDOM(
@@ -13,16 +13,35 @@ const dom = new JSDOM(
   <!-- From: https://github.com/lgarron/minimal-html-style (v1.0.0) -->
   <meta name="viewport" content="width=device-width, initial-scale=0.75">
   <style>
+    :root {
+      color-scheme: light dark;
+    }
+
     html {
       font-family: -apple-system, Roboto, Ubuntu, Tahoma, sans-serif;
-      font-size: 1.25rem; padding: 2em;
-      display: grid; justify-content: center;
+      font-size: 1.25rem;
+      padding: 2em;
+      display: grid;
+      justify-content: center;
     }
-    body { width: 100%; max-width: 40em; margin: 0; }
-    @media (prefers-color-scheme: dark) {
-      html { background: #000D; color: #EEE; }
-      a { color: #669df5; }
-      a:visited { color: #af73d5; }
+
+    body {
+      width: 100%;
+      max-width: 40em;
+      margin: 0;
+    }
+
+    table a:not(:hover) {
+      text-decoration: none;
+    }
+
+    td {
+        border: 1px solid;
+        padding: 0.5em;
+    }
+
+    table {
+      border-collapse: collapse;
     }
   </style>
 </head>
@@ -30,8 +49,17 @@ const dom = new JSDOM(
 <body>
   <!--h1></h1-->
   <p>Files</p>
-  <ul>
-  </ul>
+  <table>
+    <thead>
+      <tr>
+        <th>File</th>
+        <th>Size</th>
+      </tr>
+    </thead>
+    <tbody></tbody>
+  </table>
+  <br>
+  <p>Files are encoded using HEVC. <a href="https://lgarron-public-media.s3.amazonaws.com/about/HEVC.html">See here for more details.</a></p>
 </body>
 
 </html>
@@ -40,22 +68,26 @@ const dom = new JSDOM(
 
 const { document, XMLSerializer } = dom.window;
 
-const ul = document.querySelector("ul");
-let firstChild = true;
-for (const fileName of argv.slice(2)) {
-  ul.append("  ");
-  if (firstChild) {
-    firstChild = false;
-  } else {
-    ul.append("  ");
-  }
-  const li = ul.appendChild(document.createElement("li"));
-  const a = li.appendChild(document.createElement("a"));
-  a.href = fileName;
+const GiB = 1024 ** 3;
+function showGiB(bytes: number): string {
+  const deciGiB = Math.floor((bytes / GiB) * 10);
+  return `${Math.floor(deciGiB / 10)}.${Math.floor(deciGiB % 10)}\xa0GiB`;
+}
+
+const tbody = document.querySelector("tbody");
+// The list of movie extensions is hardcoded for now.
+// TODO: warn on less compatible extensions.
+for await (const fileName of new Glob("*.{mov,mp4}").scan()) {
+  const tr = tbody.appendChild(document.createElement("tr"));
+  const filenameTD = tr.appendChild(document.createElement("td"));
+  const a = filenameTD.appendChild(document.createElement("a"));
+  a.href = encodeURI(fileName);
+  a.download = fileName;
   const code = a.appendChild(document.createElement("code"));
   code.textContent = fileName;
-  ul.append("\n");
+  tbody.append("\n");
+  const fileSizeTD = tr.appendChild(document.createElement("td"));
+  fileSizeTD.append(showGiB(Bun.file(fileName).size));
 }
-ul.append("  ");
 
 console.log(new XMLSerializer().serializeToString(document));
