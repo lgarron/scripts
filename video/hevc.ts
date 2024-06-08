a9f711dc6a1e16353ca723645006186bfe285b3f
Lucas Garron <code@garron.net>
2024-06-07 19:32:55 -0700

    [hevc] Use a simplified codec fingerprint.

--- video/hevc.ts
+++ video/hevc.ts
@@ -59,59 +59,47 @@ const videoStream: FFprobeStream = (() => {
   exit(1);
 })();
 
-const codec_fingerprint = `${videoStream.codec_name}/${videoStream.pix_fmt}/${videoStream.color_space}/${videoStream.color_primaries}/${videoStream.color_transfer}`;
+// const codec_fingerprint = `${videoStream.codec_name}/${videoStream.pix_fmt}/${videoStream.color_space}/${videoStream.color_primaries}/${videoStream.color_transfer}`;
+const simplifiedCodecFingerprint = `${videoStream.pix_fmt}/${videoStream.color_transfer}`;
 
-console.log(codec_fingerprint);
+console.log(simplifiedCodecFingerprint);
 
 const handbrakePreset = await (async () => {
-  switch (codec_fingerprint) {
-    // biome-ignore format: https://github.com/biomejs/biome/issues/2786
-    case "hevc/yuv422p10le/bt2020nc/bt2020/arib-std-b67": // Final cut HLG export?
-    case "av1/yuv420p10le/bt2020nc/bt2020/arib-std-b67": // YouTube
-    case "vp9/yuv420p10le/bt2020nc/bt2020/arib-std-b67": {
-      // YouTube (HLG)
+  switch (simplifiedCodecFingerprint) {
+    case "yuv422p10le/arib-std-b67":
+    case "yuv420p10le/arib-std-b67": {
       console.log("Detected 10-bit HDR footage.");
       return "HEVC 10-bit (qv65)";
     }
-    case "mpeg2video/yuv420p/smpte170m/smpte170m/smpte170m": // DVD?
-    case "h264/yuv420p/bt470bg/bt470bg/smpte170m": // Oculus Quest 1 screen captures
-    case "vp9/yuv420p/bt709/undefined/undefined": // ???
-    case "vp9/yuv420p/bt709/bt709/bt709": // YouTube (SDR)
-    case "h264/yuv420p/smpte170m/bt470bg/bt709": // ???
-    case "hevc/yuvj420p/bt709/bt709/bt709":
-    case "hevc/yuv420p/bt709/bt709/bt709": // Final Cut export
-    case "h264/yuvj420p/smpte170m/smpte432/bt709": // iPhone 15 Pro
-    // biome-ignore format: https://github.com/biomejs/biome/issues/2786
-    case "h264/yuv420p/bt709/bt709/bt709": // macOS Screen capture
-  {
-    console.log("Detected 8-bit SDR (or SDR-mapped) footage.");
-    return "HEVC 8-bit (qv65)";
-  }
-    // biome-ignore format: https://github.com/biomejs/biome/issues/2786
-    case  "hevc/yuv420p10le/bt709/bt709/bt709": // `hevc`'s own output
-  {
-    console.warn(`
+    case "yuv420p/smpte170m":
+    case "yuv420p/bt709":
+    case "yuvj420p/bt709": {
+      console.log("Detected 8-bit SDR (or SDR-mapped) footage.");
+      return "HEVC 8-bit (qv65)";
+    }
+    case "yuv420p10le/bt709":
+    case "yuv420p10le/undefined": {
+      // `hevc`'s own output
+      console.warn(`
 Detected \`hevc\`'s own output with an accidental 10-bit encoding for 8-bit video data. This is not an issue if you expected it, but you may want to run \`hevc\` on the original source if it's still available.
 `);
-    console.write("Continuing.");
-    await sleep(1000);
-    console.write(".");
-    await sleep(1000);
-    console.write(".");
-    await sleep(1000);
-    console.log();
-    // biome-ignore lint/suspicious/noFallthroughSwitchClause: Intentional
-    // fallthrough
-  }
-    case "hevc/yuv420p10le/bt709/undefined/undefined": // Final Cut export?
-    case "h264/yuv422p10le/bt709/undefined/undefined": // R5 C using XF-AVC
-    case "hevc/yuv422p10le/bt709/undefined/undefined": {
-      // biome-ignore format: https://github.com/biomejs/biome/issues/2786 // R5 C HEVC (e.g. BT.709 or CLog 3)
+      console.write("Continuing.");
+      await sleep(1000);
+      console.write(".");
+      await sleep(1000);
+      console.write(".");
+      await sleep(1000);
+      console.log();
+      // biome-ignore lint/suspicious/noFallthroughSwitchClause: Intentional fallthrough
+    }
+    case "yuv422p10le/undefined": {
       console.log("Detected 10-bit SDR (or SDR-mapped) footage.");
       return "HEVC 10-bit (qv65)";
     }
     default: {
-      throw new Error(`Unknown codec fingerprint: ${codec_fingerprint}`);
+      throw new Error(
+        `Unknown simplifiedCodecFingerprint: ${simplifiedCodecFingerprint}`,
+      );
     }
   }
 })();
@@ -146,4 +134,4 @@ if (
   throw new Error();
 }
 
-// TODO: catch Ctrl-c and rename to indicate partial transcoding
+// TODO: catch Ctrl-C and rename to indicate partial transcoding
