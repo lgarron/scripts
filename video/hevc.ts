e8295b3debec3f00d988cd5424d42cc3933e4ef4
Lucas Garron <code@garron.net>
2024-06-18 23:50:50 -0700

    [hevc] Add a `--force-bit-depth` option and print `HandBrakeCLI` command.

--- video/hevc.ts
+++ video/hevc.ts
@@ -4,18 +4,24 @@ import { exit } from "node:process";
 import { parseArgs } from "node:util";
 import { $, file, sleep, spawn } from "bun";
 
+const HANDBRAKE_8_BIT_DEPTH_PRESET = "HEVC 8-bit (qv65)";
+const HANDBRAKE_10_BIT_DEPTH_PRESET = "HEVC 10-bit (qv65)";
+
 const { values: options, positionals } = parseArgs({
   options: {
     quality: { type: "string", default: "65" },
+    "force-bit-depth": { type: "string" },
     help: { type: "boolean" },
   },
   allowPositionals: true,
 });
 
 function printHelp() {
-  console.info(`Usage: hevc [--quality VALUE] <INPUT-FILE>
+  console.info(`Usage: hevc [--quality VALUE] [--force-bit-depth VALUE] <INPUT-FILE>
 
-Default quality is 65.`);
+Default quality is 65.
+Forced bit depth can be 8 or 10.
+`);
 }
 
 if (options.help) {
@@ -64,38 +70,57 @@ const simplifiedCodecFingerprint = `${videoStream.pix_fmt}/${videoStream.color_t
 
 console.log(simplifiedCodecFingerprint);
 
-const handbrakePreset = await (async () => {
+const forcedBitDepth: 8 | 10 | null = (() => {
+  const { "force-bit-depth": forceBitDepth } = options;
+  if (!forceBitDepth) {
+    return null;
+  }
+  const parsedForceBitDepth = Number.parseInt(forceBitDepth);
+  if (![8, 10].includes(parsedForceBitDepth)) {
+    console.log("Invalid forced bit depth specified.");
+    exit(1);
+  }
+  return parsedForceBitDepth as 8 | 10;
+})();
+
+let bitDepth: 8 | 10 = await (async () => {
   switch (simplifiedCodecFingerprint) {
     case "yuv422p10le/arib-std-b67":
     case "yuv420p10le/arib-std-b67": {
       console.log("Detected 10-bit HDR footage.");
-      return "HEVC 10-bit (qv65)";
+      return 10;
     }
     case "yuv420p/smpte170m":
     case "yuv420p/bt709":
     case "yuvj420p/bt709":
     case "yuv444p/undefined": {
       console.log("Detected 8-bit SDR (or SDR-mapped) footage.");
-      return "HEVC 8-bit (qv65)";
+      return 8;
     }
+    case "yuv422p10le/bt709":
     case "yuv420p10le/bt709":
     case "yuv420p10le/undefined": {
-      // `hevc`'s own output
-      console.warn(`
-Detected \`hevc\`'s own output with an accidental 10-bit encoding for 8-bit video data. This is not an issue if you expected it, but you may want to run \`hevc\` on the original source if it's still available.
-`);
-      console.write("Continuing.");
-      await sleep(1000);
-      console.write(".");
-      await sleep(1000);
-      console.write(".");
-      await sleep(1000);
-      console.log();
-      // biome-ignore lint/suspicious/noFallthroughSwitchClause: Intentional fallthrough
+      if (forcedBitDepth) {
+        console.log(
+          `Detected an input with 10-bit encoding for BT.709 video data. Using an output bit depth of ${forcedBitDepth} from the specified --force-bit-depth option.`,
+        );
+        console.write("Continuing in 2 seconds");
+        for (let i = 0; i < 3; i++) {
+          await sleep(500);
+          console.write(".");
+        }
+        await sleep(500);
+        return forcedBitDepth;
+      }
+      console.warn(
+        "Detected an input with 10-bit encoding for BT.709 video data. You must specify the --force-bit-depth option.",
+      );
+      exit(1);
+      break; // Workaround for: https://github.com/biomejs/biome/issues/3235
     }
     case "yuv422p10le/undefined": {
       console.log("Detected 10-bit SDR (or SDR-mapped) footage.");
-      return "HEVC 10-bit (qv65)";
+      return 10;
     }
     default: {
       throw new Error(
@@ -105,32 +130,59 @@ Detected \`hevc\`'s own output with an accidental 10-bit encoding for 8-bit vide
   }
 })();
 
+let bitDepthFileComponent = "";
+if (forcedBitDepth) {
+  console.warn(
+    `Forced bit depth of ${forcedBitDepth} was specified. Overriding…`,
+  );
+  bitDepth = forcedBitDepth as 8 | 10;
+  bitDepthFileComponent = `.${forcedBitDepth}-bit`;
+}
+
+const handbrakePreset = (() => {
+  switch (bitDepth) {
+    case 8:
+      return HANDBRAKE_8_BIT_DEPTH_PRESET;
+    case 10:
+      return HANDBRAKE_10_BIT_DEPTH_PRESET;
+    default:
+      throw new Error("Bit depth does not correspond to a HandBrake preset.");
+  }
+})();
+
 let destPrefix = `${inputFile}.hevc.qv${quality}`;
 if (await file(`${destPrefix}.mp4`).exists()) {
-  destPrefix = `${inputFile}.hevc.qv${quality}.${new Date()
+  destPrefix = `${inputFile}.hevc${bitDepthFileComponent}.qv${quality}.${new Date()
     .toISOString()
     .replaceAll(":", "-")}`;
 }
 const dest = `${destPrefix}.mp4`;
 
+const command = [
+  "HandBrakeCLI",
+  "--preset-import-file",
+  "/Users/lgarron/Code/git/github.com/lgarron/dotfiles/exported/HandBrake/UserPresets.json", // TODO
+  "--preset",
+  handbrakePreset,
+  "--quality",
+  quality.toString(),
+  "-i",
+  inputFile,
+  "-o",
+  dest,
+];
+
+console.log("");
+console.log("Running command:");
+console.log("");
+console.write('"');
+console.write(command.join('" \\\n  "'));
+console.log('"');
+console.log("");
+
 if (
   // TODO: transfer HiDPI hint (e.g. for screencaps)
-  (await spawn(
-    [
-      "HandBrakeCLI",
-      "--preset-import-file",
-      "/Users/lgarron/Code/git/github.com/lgarron/dotfiles/exported/HandBrake/UserPresets.json", // TODO
-      "--preset",
-      handbrakePreset,
-      "--quality",
-      quality.toString(),
-      "-i",
-      inputFile,
-      "-o",
-      dest,
-    ],
-    { stdout: "inherit", stderr: "inherit" },
-  ).exited) !== 0
+  (await spawn(command, { stdout: "inherit", stderr: "inherit" }).exited) !== 0
 ) {
   throw new Error();
 }
