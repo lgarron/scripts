54d49e25231902f3872c61baa4b6bf0f4856b8cd
Lucas Garron <code@garron.net>
2024-10-11 12:48:14 -0700

    [openscad-auto] Add low-fi guard.

--- app-tools/openscad-auto.fish
+++ app-tools/openscad-auto.fish
@@ -8,13 +8,22 @@ end
 set INPUT_FILE $argv[1]
 set OUTPUT_FILE $INPUT_FILE.stl
 
+set LOW_FI_FALSE "LOW_FI_DEV = false;"
+if cat $INPUT_FILE | grep "^"$LOW_FI_FALSE > /dev/null
+  echo "✅ "$LOW_FI_FALSE
+else
+  echo "Could not verify that the file is set to hi-fi. Please make sure the following line is present:"
+  echo (set_color --bold)$LOW_FI_FALSE(set_color normal)
+  exit 1
+end
+
 if test -e $OUTPUT_FILE
   set -l OUTPUT_FILE_BACKUP $OUTPUT_FILE.bak.(date "+%Y-%m-%d_%H-%M-%S").stl
-  echo "Output file already exists. Moving to: "(set_color --bold)"$OUTPUT_FILE_BACKUP"(set_color normal)
+  echo "➡️ Output file already exists. Moving to: "(set_color --bold)"$OUTPUT_FILE_BACKUP"(set_color normal)
   mv $OUTPUT_FILE $OUTPUT_FILE_BACKUP
 end
 
-echo "Writing new file to: "(set_color --bold)"$INPUT_FILE.stl"(set_color normal)
+echo "✍️ Writing new file to: "(set_color --bold)"$INPUT_FILE.stl"(set_color normal)
 openscad -o $INPUT_FILE.stl $INPUT_FILE
 
 terminal-notifier -title "openscad-auto" -message "Done converting: $INPUT_FILE" -execute "open -R "(string escape $OUTPUT_FILE)""
