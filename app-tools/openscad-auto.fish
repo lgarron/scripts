70f98153766337689190590fc2a64d555b4429ca
Lucas Garron <code@garron.net>
2024-10-11 12:21:47 -0700

    [openscad-auto] Auto-backup existing file.

--- app-tools/openscad-auto.fish
+++ app-tools/openscad-auto.fish
@@ -8,7 +8,13 @@ end
 set INPUT_FILE $argv[1]
 set OUTPUT_FILE $INPUT_FILE.stl
 
-echo "Writing to: "(set_color --bold)"$INPUT_FILE.stl"(set_color normal)
+if test -e $OUTPUT_FILE
+  set -l OUTPUT_FILE_BACKUP $OUTPUT_FILE.bak.(date "+%Y-%m-%d_%H-%M-%S").stl
+  echo "Output file already exists. Moving to: "(set_color --bold)"$OUTPUT_FILE_BACKUP"(set_color normal)
+  mv $OUTPUT_FILE $OUTPUT_FILE_BACKUP
+end
+
+echo "Writing new file to: "(set_color --bold)"$INPUT_FILE.stl"(set_color normal)
 openscad -o $INPUT_FILE.stl $INPUT_FILE
 
 terminal-notifier -title "openscad-auto" -message "Done converting: $INPUT_FILE" -execute "open -R "(string escape $OUTPUT_FILE)""
