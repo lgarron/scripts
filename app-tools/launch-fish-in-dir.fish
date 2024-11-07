b8cd03c384d904ef57e566886c7c26b69026f563
Lucas Garron <code@garron.net>
2024-11-06 16:09:24 -0800

    [launch-fish-in-dir] Update `fish` path.

--- app-tools/launch-fish-in-dir.fish
+++ app-tools/launch-fish-in-dir.fish
@@ -1,3 +1,3 @@
-#!/Users/lgarron/.fish-master/bin/fish --no-config
+#!/opt/homebrew/bin/fish --no-config
 
-/Users/lgarron/.fish-master/bin/fish --interactive --init-command "cd-dir-from-iterm "(string escape $argv[1])
+/opt/homebrew/bin/fish --interactive --init-command "cd-dir-from-iterm "(string escape $argv[1])
